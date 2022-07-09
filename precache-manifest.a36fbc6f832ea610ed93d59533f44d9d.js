59d82ba9d190a0768fc6952974de77a40acf9ddb
kumarshivam <kumarshivam95074@gmail.com>
2022-07-10 01:27:32 +0530

    Updates

---
+++ precache-manifest.a36fbc6f832ea610ed93d59533f44d9d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42231a6d0b2aead47c9e3fa455d54d41",
+    "url": "/Shivamkumar/index.html"
+  },
+  {
+    "revision": "9717d0e0da93299c0c62",
+    "url": "/Shivamkumar/static/css/2.14351861.chunk.css"
+  },
+  {
+    "revision": "d734a91ff2f9832b18b7",
+    "url": "/Shivamkumar/static/css/main.2db685a0.chunk.css"
+  },
+  {
+    "revision": "9717d0e0da93299c0c62",
+    "url": "/Shivamkumar/static/js/2.7da599ff.chunk.js"
+  },
+  {
+    "revision": "76c33606446526c6ec38bc107ec65a87",
+    "url": "/Shivamkumar/static/js/2.7da599ff.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d734a91ff2f9832b18b7",
+    "url": "/Shivamkumar/static/js/main.012fda4b.chunk.js"
+  },
+  {
+    "revision": "a0a20e0f00f67e267041",
+    "url": "/Shivamkumar/static/js/runtime-main.725fd81b.js"
+  },
+  {
+    "revision": "dc642748fd57676acc5dd9f3d5f0bf69",
+    "url": "/Shivamkumar/static/media/Hello.dc642748.jpg"
+  },
+  {
+    "revision": "38828042bce3def5dbc3c5b4a5d2df93",
+    "url": "/Shivamkumar/static/media/Pencil.38828042.jpg"
+  },
+  {
+    "revision": "ea39cfa913576f5987c23354c7da963e",
+    "url": "/Shivamkumar/static/media/laptop1.ea39cfa9.jpg"
+  },
+  {
+    "revision": "4d3c0ac50b6c8d471da024dc07934039",
+    "url": "/Shivamkumar/static/media/sam.4d3c0ac5.jpg"
+  }
+]);
